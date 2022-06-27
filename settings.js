61c67ad5c80047615a0c7b46f5ec4415157db9fa
Pinzz45 <108239578+Pinzz45@users.noreply.github.com>
2022-06-27 16:53:59 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,25 +19,25 @@
   }
 
   // Setting Owner
-  global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-  global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-  global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+  global.owner = ['6287734910547','6287734910547','6287734910547'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+  global.premium = ['6287734910547'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+  global.ownernomer = '6287734910547' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
   // Set Nama Own & Bot
-  global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+  global.ownername = 'Vinn' //ubah jadi nama mu, note tanda ' gausah di hapus!
   global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-  global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+  global.footer = 'Vinn' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
   // Set Kebutuhan Button & Kontak
   //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
   global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
-  global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
+  global.mygc = 'https://chat.whatsapp.com/C0cERkbxNmM2veEQSKPf7D'
   global.myweb = 'https://api-riychdwayne.herokuapp.com'
   global.region = 'indonesia'
 
   // Set Wm
   global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-  global.author = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+  global.author = 'Vinn' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
   // Set Nama Session
   //gausah di apa² in!
